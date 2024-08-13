6ddd35dbe5e035fcf78e3e013ba86adc23f68d3f
Vishakha Binani <142291827+vishakhabinani@users.noreply.github.com>
2024-08-13 17:12:15 +0530

    Update index.js
    Add correct file path for sounds

--- index.js
+++ index.js
@@ -15,31 +15,31 @@ function handleClick(){
 function makesound(key){
    switch(key) {
     case "w":
-        var tom1=new Audio('sounds/tom-1.mp3');
+        var tom1=new Audio('./tom-1.mp3');
         tom1.play();
         break;
     case "a":
-        var tom2=new Audio('sounds/tom-2.mp3');
+        var tom2=new Audio('./tom-2.mp3');
         tom2.play();
         break;
     case "s":
-        var tom3=new Audio('sounds/tom-3.mp3');
+        var tom3=new Audio('./tom-3.mp3');
         tom3.play();
         break;
     case "d":
-        var tom4=new Audio('sounds/tom-4.mp3');
+        var tom4=new Audio('./tom-4.mp3');
         tom4.play();
         break;
     case "j":
-        var snare=new Audio('sounds/snare.mp3');
+        var snare=new Audio('./snare.mp3');
         snare.play();
         break;
     case "k":
-        var flash=new Audio('sounds/crash.mp3');
+        var flash=new Audio('./crash.mp3');
         flash.play();
         break;
     case "l":
-        var kick=new Audio('sounds/kick-bass.mp3');
+        var kick=new Audio('./kick-bass.mp3');
         kick.play();
         break;
    }
